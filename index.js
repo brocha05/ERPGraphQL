065b37243855d28d93575a9043f39bc5b98c47e0
brocha05 <brocha_05@hotmail.com>
2023-07-05 21:50:18 -0600

    Creando schema y resolvers

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
-const {ApolloServer, gpl} = require('apollo-server')
+const {ApolloServer, gql} = require('apollo-server')
 
-const server = new ApolloServer();
+// Mock Data
+const cursos = [
+    {
+        titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
+        tecnologia: 'JavaScript ES6',
+    },
+    {
+        titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+        tecnologia: 'React',
+    },
+    {
+        titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+        tecnologia: 'Node.js'
+    }, 
+    {
+        titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+        tecnologia: 'React'
+    }
+];
 
-server.listen().then( (url) => {
+// Schema
+const typeDefs = gql`
+    type Curso {
+        titulo: String
+        tecnologia: String
+    }
+
+    type Tecnologia {
+        tecnologia: String
+    }
+
+    type Query {
+        obtenerCursos: [Curso]
+        obtenerTecnologias: [Tecnologia]
+    }
+`;
+
+// Resolvers
+const resolvers = {
+    Query: {
+        obtenerCursos: () => cursos,
+        obtenerTecnologias: () => cursos
+    }
+}
+
+// Servidor
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+// Arrancar el servidor
+server.listen().then( ({url}) => {
     console.log(`Server running in URL ${url}`)
 } )
